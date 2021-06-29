49bccba14627223ab68ddf5b21db9aa342d0554a
carterhanks <carterhanks@gmail.com>
2021-06-29 12:31:18 -0600

    Redeploy

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar');
+var rollbar = new Rollbar({
+  accessToken: '9898eecc5ca74304810e1f4a021da3f0',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!');
 
 app.use(express.json());
 
 app.get('/', function(req, res) {
+    rollbar.log('Hello World');
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
